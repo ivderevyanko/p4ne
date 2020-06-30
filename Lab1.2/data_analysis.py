d3b3bdd2d72b905e4aa6ac74729a7bca63ba24cc
iv.derevyanko <iv.derevyanko@jet.su>
2020-06-30 18:31:07 +0300

    Lab 1.2 done

--- Lab1.2/data_analysis.py
+++ Lab1.2/data_analysis.py
@@ -2,21 +2,20 @@ from matplotlib import pyplot
 from openpyxl import load_workbook
 
 wb = load_workbook('data_analysis_lab.xlsx')
-
 sheet = wb['Data']
 
 sheet['A'][1:]
-sheet['B'][1:]
 sheet['C'][1:]
+sheet['D'][1:]
 
 def getvalue(x):
     return x.value
 
 year = list(map(getvalue, sheet['A'][1:]))
-temper = list(map(getvalue, sheet['B'][1:]))
-act = list(map(getvalue, sheet['C'][1:]))
+temper = list(map(getvalue, sheet['C'][1:]))
+act = list(map(getvalue, sheet['D'][1:]))
 
-pyplot.plot(year, temper, label="Метка")
-pyplot.plot(year, act, label="Метка")
+pyplot.plot(year, temper, label="Относит. темп-ра")
+pyplot.plot(year, act, label="Активность")
 
 pyplot.show()
